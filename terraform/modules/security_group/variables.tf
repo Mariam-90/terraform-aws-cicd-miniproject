cd903fcedf95937afa94854d08300e7c43def3ca
mariam-90 <129694311+maryam19455@users.noreply.github.com>
2026-08-18 12:28:18 +0300

    Allowed ssh cidr

--- terraform/modules/security_group/variables.tf
+++ terraform/modules/security_group/variables.tf
@@ -11,7 +11,9 @@ variable "project_name" {
 
 variable "my_ip_cidr" {
   type        = string
-  description = "Your public IP address in CIDR format for SSH access"
+  description = "CIDR block allowed to access SSH"
+  default     = "0.0.0.0/0"
+
   validation {
     condition     = can(cidrhost(var.my_ip_cidr, 0))
     error_message = "my_ip_cidr must be a valid CIDR block, e.g. 1.2.3.4/32."
